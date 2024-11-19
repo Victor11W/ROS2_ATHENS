496a43227a11518a3cad8932039968b4bc19b127
VictorW <athens@ROSCourseAthens.myguest.virtualbox.org>
2024-11-19 13:41:33 +0100

    enregistrements

---
+++ src/build/first_interfaces/CMakeFiles/first_interfaces__cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/first_interfaces__cpp"
+  "rosidl_generator_cpp/first_interfaces/msg/detail/example_message__builder.hpp"
+  "rosidl_generator_cpp/first_interfaces/msg/detail/example_message__struct.hpp"
+  "rosidl_generator_cpp/first_interfaces/msg/detail/example_message__traits.hpp"
+  "rosidl_generator_cpp/first_interfaces/msg/detail/example_message__type_support.hpp"
+  "rosidl_generator_cpp/first_interfaces/msg/example_message.hpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/first_interfaces__cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/first_package/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/athens/ros2_athens_ws/src/install/first_interfaces/share/first_interfaces/package.sh"
