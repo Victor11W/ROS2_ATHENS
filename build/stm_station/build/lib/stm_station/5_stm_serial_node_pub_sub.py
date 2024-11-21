2efafbe338e35ac40a9cf4cbc2b7c3f014cc2f5e
VictorW <athens@ROSCourseAthens.myguest.virtualbox.org>
2024-11-21 15:56:10 +0100

    merge des modifications qui ont réglé le pb avec l'utilisation du port serial pour utiliser avec le moteur

--- build/stm_station/build/lib/stm_station/5_stm_serial_node_pub_sub.py
+++ build/stm_station/build/lib/stm_station/5_stm_serial_node_pub_sub.py
@@ -7,7 +7,7 @@
 import rclpy
 from rclpy.node import Node
 from stm_interfaces.msg import STMControl, STMState
-## import serial
+import serial
 
 
 # 2. Define the SerialPubSubNode class
@@ -19,13 +19,13 @@ class SerialPubSubNode(Node):
         super().__init__('stm_serial_node')
 
         # 3. TODO: Get Serial Configuration Values 
-        ## self.port = '/dev/ttyACM0'  # Serial port name
-        ## self.baudrate = 115200      # Baud rate for serial communication
-        ## self.loop_frequency = 1000  # Frequency to read data from the serial port
+        self.port = '/dev/ttyACM0'  # Serial port name
+        self.baudrate = 115200      # Baud rate for serial communication
+        self.loop_frequency = 1000  # Frequency to read data from the serial port
 
         # 4. Set up the serial port for communication
-        ## self.serial_port = serial.Serial(self.port, self.baudrate, timeout=0.01)
-        ##self.get_logger().info(f'Node initialized with serial port {self.port} at {self.baudrate} baud.')
+        self.serial_port = serial.Serial(self.port, self.baudrate, timeout=0.01)
+        self.get_logger().info(f'Node initialized with serial port {self.port} at {self.baudrate} baud.')
 
         # 5. TODO: Create a publisher to publish the received data from the serial port
         # using Float32MultiArray messages from the std_msgs package in the topic '/stm_state'
@@ -55,63 +55,62 @@ class SerialPubSubNode(Node):
         Periodically reads data from the serial port and publishes it as a Float32MultiArray.
         """
         # Check if data is available to read (in_waiting is the number of bytes in the input buffer)
-        # if self.serial_port.in_waiting > 0:
-        #     try:
-        #         # Read the data from the serial port 
-        #         serial_data_raw = self.serial_port.readline()
+        if self.serial_port.in_waiting > 0:
+            try:
+                # Read the data from the serial port 
+                serial_data_raw = self.serial_port.readline()
 
-        #         # Decode the data
-        #         serial_data_decoded = serial_data_raw.decode('utf-8')
+                # Decode the data
+                serial_data_decoded = serial_data_raw.decode('utf-8')
 
-        #         # Remove leading and trailing whitespace
-        #         serial_data = serial_data_decoded.strip()
+                # Remove leading and trailing whitespace
+                serial_data = serial_data_decoded.strip()
 
-        #         # Check if data is properly formatted with braces
-        #         # Spected format: 
-        #         # {motor_position, motor_velocity, imu_acceleration_x, imu_acceleration_y, imu_acceleration_z, imu_gyro_x, imu_gyro_y, imu_gyro_z}
-        #         if serial_data.startswith('{') and serial_data.endswith('}'):
-        #             serial_data = serial_data[1:-1]  # Remove the braces
-        #         else:
-        #             self.get_logger().warn('Data does not start and end with braces')
-        #             return
+                # Check if data is properly formatted with braces
+                # Spected format: 
+                # {motor_position, motor_velocity, imu_acceleration_x, imu_acceleration_y, imu_acceleration_z, imu_gyro_x, imu_gyro_y, imu_gyro_z}
+                if serial_data.startswith('{') and serial_data.endswith('}'):
+                    serial_data = serial_data[1:-1]  # Remove the braces
+                else:
+                    self.get_logger().warn('Data does not start and end with braces')
+                    return
                 
-        #         # Split the data by commas
-        #         float_values = serial_data.split(',')
-        float_values = (1,2,3,4,5,6,7,8)
+                # Split the data by commas
+                float_values = serial_data.split(',')
 
-        if len(float_values) == 8:
-            # Ensure the correct number of data parts is present (expected 8 fields)
-            # 1. motor_position
-            # 2. motor_velocity
-            # 3. imu_acceleration_x
-            # 4. imu_acceleration_y
-            # 5. imu_acceleration_z
-            # 6. imu_gyro_x
-            # 7. imu_gyro_y
-            # 8. imu_gyro_z
+                if len(float_values) == 8:
+                    # Ensure the correct number of data parts is present (expected 8 fields)
+                    # 1. motor_position
+                    # 2. motor_velocity
+                    # 3. imu_acceleration_x
+                    # 4. imu_acceleration_y
+                    # 5. imu_acceleration_z
+                    # 6. imu_gyro_x
+                    # 7. imu_gyro_y
+                    # 8. imu_gyro_z
 
-            # Create a Float32MultiArray message
-            # TODO: Create and populate the Float32MultiArray message
-            STM_state_msg = STMState()
-            STM_state_msg.motor_encoder = float(float_values[0])
-            STM_state_msg.motor_velocity = float(float_values[1])
-            STM_state_msg.accel_x = float(float_values[2])
-            STM_state_msg.accel_y = float(float_values[3])
-            STM_state_msg.accel_z = float(float_values[4])
-            STM_state_msg.gyro_x = float(float_values[5])
-            STM_state_msg.gyro_y = float(float_values[6])
-            STM_state_msg.gyro_z = float(float_values[7])
-
+                    # Create a Float32MultiArray message
+                    # TODO: Create and populate the Float32MultiArray message
+                    STM_state_msg = STMState()
+                    STM_state_msg.motor_encoder = float(float_values[0])
+                    STM_state_msg.motor_velocity = float(float_values[1])
+                    STM_state_msg.accel_x = float(float_values[2])
+                    STM_state_msg.accel_y = float(float_values[3])
+                    STM_state_msg.accel_z = float(float_values[4])
+                    STM_state_msg.gyro_x = float(float_values[5])
+                    STM_state_msg.gyro_y = float(float_values[6])
+                    STM_state_msg.gyro_z = float(float_values[7])
 
 
-            # Publish the message
-            # TODO: Publish the STM_state_msg
-            self.stm_state_publisher.publish(STM_state_msg) #rajout
-            
 
-        self.get_logger().info(f'Published data: {float_values}')
-            # except ValueError as e:
-            #     self.get_logger().warn(f'Error parsing serial data: {e}')
+                    # Publish the message
+                    # TODO: Publish the STM_state_msg
+                    self.stm_state_publisher.publish(STM_state_msg) #rajout
+                    
+
+                self.get_logger().info(f'Published data: {float_values}')
+            except ValueError as e:
+                self.get_logger().warn(f'Error parsing serial data: {e}')
 
 
     # 8. Define the callback function for the timer
@@ -127,7 +126,7 @@ class SerialPubSubNode(Node):
             control_data = f"{{{msg.control_type}, {msg.setpoint}, {msg.kp}, {msg.pwm}}}"
 
             # TODO: Encode the control data as UTF-8 and write it to the serial port 
-            ## self.serial_port.write((control_data+'\n').encode('utf-8'))
+            self.serial_port.write((control_data+'\n').encode('utf-8'))
             self.get_logger().info(f'Sent control command: {control_data}')
             
         except Exception as e:
@@ -138,7 +137,7 @@ class SerialPubSubNode(Node):
         """
         Clean up the node by closing the serial port.
         """
-        ## self.serial_port.close()
+        self.serial_port.close()
         super().destroy_node()
 
 def main(args=None):
